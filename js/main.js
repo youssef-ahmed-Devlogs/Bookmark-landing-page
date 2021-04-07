8f7110ab121593e81c61f07fbcdb2bba6b987c10
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2021-04-07 23:00:39 +0200

    Add file json and use ajax to get questions data and insert to html width jQuery & ajax

--- js/main.js
+++ js/main.js
@@ -24,11 +24,38 @@ $(function() {
         $("." + $(this).attr("id")).fadeIn(600).siblings().hide();
     });
 
-    // Frequently Asked Questions
-    $(".asked-questions-content .question").on("click", function() {
-        let id = $("." + $(this).attr("id"));
-        id.slideToggle(500);
-        $("#" + $(this).attr("id") + " .icon-arrow").toggleClass("close");
-    });
+    // Frequently Asked Questions >> Ajax to get Questions Data
+    $.ajax({
+        type: "GET",
+        url: "questions.json",
+        data: {
+            format: "json",
+        },
+        error: function() {
+            console.log("Error : Questiosns Api Not Found");
+        },
+        success: function(data) {
+            
+            let questions = data.map(question => {
+                return `
+                <li class="question ${question.number}" id="${question.id}">
+                <div class="question-title">${question.title} <div class="icon-arrow open"></div></div>
+                <p class="answer ${question.id}">
+                ${question.answer}
+                </p>
+                </li>
+                `
+            });
 
+            $(".asked-questions-content ul").html(questions.join(""));
+
+            // Frequently Asked Questions >> Open & Close
+            $(".asked-questions-content .question").on("click", function() {
+                let id = $("." + $(this).attr("id"));
+                id.slideToggle(500);
+                $("#" + $(this).attr("id") + " .icon-arrow").toggleClass("close");
+                console.log("ok");
+            });
+        }
+    })
 });
